ab79a7f53bd170da94afc45063ad88964d0b04a8
navya1227 <kandukurinavya03@gmail.com>
2025-11-06 17:58:28 +0530

    backend code initial

---
+++ backend/server.js
@@ -0,0 +1,45 @@
+//creating the server for the Nutrition food application 
+const express = require('express');
+const mongoose = require('mongoose');
+const {MongoClient} = require("mongodb");
+const app = express();
+const db_url = 'mongodb+srv://adminhar:admin123@cluster0.axogu3b.mongodb.net/?appName=Cluster0';
+// async function run(){
+//     try{
+//         const client = new MongoClient(db_url);
+//         await client.connect();
+//         const db = client.db("harshadb");
+//         const data = await db.collection('products').findAll().toArray();
+//         res.json(data);   
+//     }catch(err){
+//         console.log(err.stack)
+//     }
+// }
+// run().catch(console.dir);
+app.get('/api/products', async(req, res)=>{
+    // res.send("Hello");
+    try{
+        const client = new MongoClient(db_url);
+        await client.connect();
+        const db =  client.db('employee');
+        const data = await db.collection('demoUser').find().toArray();
+        console.log(data);
+        res.json(data);
+    }catch(err){
+        res.send(err);
+    }
+})
+
+
+
+//api for get the data
+app.get('/', (req, res)=>{
+    res.send("Wellcome! to api");
+})
+
+
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
